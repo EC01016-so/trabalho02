f64dc6ff849e388e7b4c54c35441a6db2053ca3a
JD3an <89491859+JD3an@users.noreply.github.com>
2022-10-12 16:14:01 -0300

    Update password.c

--- trabalho02.4/password.c
+++ trabalho02.4/password.c
@@ -3,14 +3,19 @@
 #include <unistd.h>
 
 int main() {
+  
+  // Variavel que armazenará o nome do usuário
   char *buf;
+  // Variavel que receberá o comando, em string, de 'passwd'
   char *pw = "passwd";
 
+  // Alocação na memória do tamanho do nome do usuário (em caracteres)
   buf = (char *)malloc(10 * sizeof(char));
+  // Recebe o nome do usuário
   buf = getlogin();
-
-  printf("\n Você está  trocando o password de um usuário: %s \n", buf);
-
+  // Exibe na tela um aviso informando que a senha do usuário será alterado
+  printf("\n Você está trocando o 'password' do usuário %s \n", buf);
+  // Chamada de sistema para executar a troca da senha
   execlp(pw, pw, buf, NULL);
 
   return 0;
