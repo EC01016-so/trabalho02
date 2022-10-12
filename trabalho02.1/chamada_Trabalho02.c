96eb6dc339be45bcc43f428a9968acf397e246a0
JD3an <89491859+JD3an@users.noreply.github.com>
2022-10-12 14:35:13 -0300

    Update chamada_Trabalho02.c

--- trabalho02.1/chamada_Trabalho02.c
+++ trabalho02.1/chamada_Trabalho02.c
@@ -10,36 +10,38 @@
 int main(int argc, char **argv){
     // Criando variavel int 
     int lista[2], concatena;
-    // Criando variable char
-    // variavel que recebe os nomes dos arquivos
+    
+    // Criando variavel char  
+    // Variaveis que receberão os nomes dos arquivos
     char *lista_07_09 = "lista_07_09.txt";
     char *lista_08_09 = "lista_08_09.txt";
     /* variavel que recebe o nome de arquivo que vai ser criado no 
-    final do excusao
-    */
+    final do execução */
     char *concatenado = "arquivo_concatenado.txt";
-    int tamanho = 2;
+    /* Variavel que alocará na memória (buffer) um espaço para o
+    conteúdo de texto dos arquivos que serão concacetenados */
     char buf[100];
-    // Abrindo o arquivo com somente letura
+    
+    // Abrindo os arquivos somente no modo leitura
     lista[0] = open(lista_07_09, O_RDONLY);
     lista[1] = open(lista_08_09, O_RDONLY);
-    /*Abrir o arquivo com somente escrever, acrescentar e se o arquivo naõ foi 
-    crear novo arquivo   */
+    // Criando novo arquivo para a concatenação
     concatena = open(concatenado, O_WRONLY |O_APPEND | O_CREAT, 0644);
 
-    // um loop para escrever na arquivo "arquivo_concatenado.txt"
+    // Um loop para escrever no arquivo "arquivo_concatenado.txt"
     if (lista[0] != -1 || lista[1] != -1 || concatena != -1){
-        for (int i=0; i<tamanho; i++){
+        for (int i=0; i<2; i++){
             read(lista[i], buf, sizeof(buf));
             write(concatena, ("%c \n", buf), strlen(buf)); 
             lseek(concatena, 0, SEEK_END);
         }
-        // fechar os arquivos que foram abertos 
+        // Fecha os arquivos que foram abertos 
         close(lista[0]);
         close(lista[1]);
         close(concatena);
-
-        printf("concatena de 'lista_07_09.txt' e 'lista_08_09.txt' foi Sucesso\n\n");
+        
+        // Exibição dos arquivos e resultados
+        printf("Concatenação de 'lista_07_09.txt' e 'lista_08_09.txt'\n\n");
         printf("Arquivo 'lista_07_09.txt'\n");
         printf("----------------------------------------------------------------------------------------\n");
         system("cat lista_07_09.txt");
@@ -55,7 +57,7 @@ int main(int argc, char **argv){
         
 
     }else{
-        // print na telar se o corre erros 
+        // Exibe na tela caso ocorra erro ao abrir os arquivos
         printf("Falha ao abrir arquivo\n");
     }
 
